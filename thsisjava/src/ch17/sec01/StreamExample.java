8681b062fb273a500f33ce2023c402a9fc30e589
jskozy <jinsu8596@gmail.com>
2023-02-02 22:18:08 +0900

    -

---
+++ thsisjava/src/ch17/sec01/StreamExample.java
@@ -0,0 +1,42 @@
+package ch17.sec01;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class StreamExample {
+
+	public static void main(String[] args) {
+		 
+		Set<String> set = new HashSet<>();
+		set.add("홍길동");
+		set.add("김길동");
+		set.add("김자바");
+
+		// 외부 반복자 이용
+		for(String item : set) {
+			System.out.println(item);
+		}
+		
+		System.out.println();
+		
+		Iterator<String> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String item = iterator.next();
+			System.out.println(item);
+		}
+		
+		// 내부 반복자 이용
+		Stream<String> stream = set.stream();
+		stream.forEach((item) -> {
+			System.out.println(item);
+		});
+		
+		System.out.println();
+		
+		stream = set.stream();
+		stream.forEach((item) -> System.out.println(item));
+	}
+
+}
